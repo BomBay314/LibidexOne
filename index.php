2f5f92b50099c17e6527c5092e75d76668c99716
BomBay314 <maksimbogomolov97@gmail.com>
2026-04-18 11:29:56 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,354 @@
+<!DOCTYPE html>
+<html lang="hi">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<link rel="shortcut icon" href="images/product-2.png" type="image/png">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<title>Libidex</title>
+
+	<style>
+		.block-1 .block-img .img-man {
+			display: none;
+		}
+
+		.block-1 .block-img .img-mob {
+			margin-top: 10px;
+		}
+
+		.block-3 .wrapper {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			width: 334px;
+			height: 250px;
+		}
+
+		.block-6 .block-img .img-man {
+			display: none;
+		}
+
+		.block-6 .block-img .img-mob {
+			margin-top: 10px;
+		}
+
+		@media (min-width:1200px) {
+			.block-1 .block-img .img-man {
+				display: block;
+			}
+
+			.block-1 .block-img .img-mob {
+				display: none;
+			}
+
+			.block-3 .wrapper {
+				width: 470px;
+				height: 100%;
+			}
+
+			.block-6 .block-img .img-man {
+				display: block;
+			}
+
+			.block-6 .block-img .img-mob {
+				display: none;
+			}
+		}
+	</style>
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript" >
+		(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+		m[i].l=1*new Date();
+		for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+		k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+		(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+	
+		ym(100107496, "init", {
+			clickmap:true,
+			trackLinks:true,
+			accurateTrackBounce:true,
+			webvisor:true
+		});
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/100107496" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
+</head>
+
+<body>
+	<div class="block-1">
+		<header class="header">
+			<div class="header__container">
+				<div class="header__logo">
+					<a href="#form">Libidex</a>
+				</div>
+				<nav class="header__menu menu">
+					<ul class="menu__list">
+						<li class="menu__item">
+							<a class="menu__link" href="#form">संघटन</a>
+						</li>
+						<li class="menu__item">
+							<a class="menu__link" href="#form">परिणाम</a>
+						</li>
+						<li class="menu__item">
+							<a class="menu__link" href="#form">गुण</a>
+						</li>
+						<li class="menu__item">
+							<a class="menu__link" href="#form">वितरण एवं भुगतान</a>
+						</li>
+					</ul>
+				</nav>
+				<div class="header__shedule"></div>
+				<button class="header__burger-btn">
+					<svg width="45" height="25" viewBox="0 0 45 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path fill-rule="evenodd" clip-rule="evenodd"
+							d="M1.798 0.104474C1.14179 0.289338 0.672802 0.593502 0.321851 1.0619C0.0470971 1.4286 0 1.5932 0 2.18639C0 2.79995 0.0414878 2.93401 0.353639 3.33024C0.572881 3.60845 0.924884 3.88465 1.27946 4.05667L1.85152 4.33415H22.38H42.9085L43.4805 4.05667C43.8351 3.88465 44.1871 3.60845 44.4064 3.33024C44.7193 2.93291 44.76 2.80068 44.76 2.17924C44.76 1.5578 44.7193 1.42557 44.4064 1.02824C44.1871 0.75003 43.8351 0.473835 43.4805 0.301809L42.9085 0.02433L22.5645 0.00489016C6.1368 -0.0107902 2.13913 0.00837457 1.798 0.104474ZM1.798 10.3747C1.14179 10.5595 0.672802 10.8637 0.321851 11.3321C0.0470971 11.6988 0 11.8634 0 12.4566C0 13.0702 0.0414878 13.2042 0.353639 13.6004C0.572881 13.8787 0.924884 14.1549 1.27946 14.3269L1.85152 14.6044H22.38H42.9085L43.4805 14.3269C43.8351 14.1549 44.1871 13.8787 44.4064 13.6004C44.7193 13.2031 44.76 13.0709 44.76 12.4494C44.76 11.828 44.7193 11.6958 44.4064 11.2985C44.1871 11.0202 43.8351 10.744 43.4805 10.572L42.9085 10.2945L22.5645 10.2751C6.1368 10.2594 2.13913 10.2786 1.798 10.3747ZM1.798 20.6449C1.14179 20.8298 0.672802 21.1339 0.321851 21.6023C0.0470971 21.969 0 22.1336 0 22.7268C0 23.3404 0.0414878 23.4744 0.353639 23.8707C0.572881 24.1489 0.924884 24.4251 1.27946 24.5971L1.85152 24.8746H22.38H42.9085L43.4805 24.5971C43.8351 24.4251 44.1871 24.1489 44.4064 23.8707C44.7193 23.4733 44.76 23.3411 44.76 22.7197C44.76 22.0982 44.7193 21.966 44.4064 21.5687C44.1871 21.2905 43.8351 21.0143 43.4805 20.8422L42.9085 20.5647L22.5645 20.5453C6.1368 20.5296 2.13913 20.5488 1.798 20.6449Z"
+							fill="#FFFFFF" />
+					</svg>
+				</button>
+			</div>
+		</header>
+		<div class="container">
+			<div class="row">
+				<div class="block-text">
+					<h1 class="title">Libidex</h1>
+					<h3 class="sub-title">अपनी क्षमता को उजागर करें और अपना आत्मविश्वास पुनः प्राप्त करें!</h3>
+					<p class="text">
+						<svg viewBox="0 0 2 126" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M1 0L0.999994 126" stroke="url(#paint0_linear_21_8)" />
+							<defs>
+								<linearGradient id="paint0_linear_21_8" x1="1.5" y1="2.18557e-08" x2="1.49999" y2="126"
+									gradientUnits="userSpaceOnUse">
+									<stop stop-color="#E99F21" />
+									<stop offset="1" stop-color="#753327" />
+								</linearGradient>
+							</defs>
+						</svg>
+						<span>पुरुषों का स्वास्थ्य पूर्ण और ऊर्जावान जीवन की कुंजी है। स्तंभन दोष न केवल अंतरंग क्षेत्र
+							पर, बल्कि आत्मविश्वास, मनोवैज्ञानिक स्थिति और साथी के साथ संबंधों पर भी नकारात्मक प्रभाव डाल
+							सकता है। Libidex एक प्राकृतिक कैप्सूल है जो पुरुष शक्ति को बहाल करने, सहनशक्ति बढ़ाने और यौन
+							जीवन की गुणवत्ता में सुधार करने में मदद करेगा।</span>
+					</p>
+				</div>
+				<div class="block-img">
+					<img src="./images/product-1.png" alt="" class="img-fluid img-product">
+					<img src="./images/1.png" alt="" class="img-fluid img-man">
+					<img src="./images/mob.png" alt="" class="img-fluid img-mob">
+				</div>
+				<div class="block-form">
+					<div class="block-price">
+						<p class="price-old">4980 INR</p>
+						<p class="price">2490 INR</p>
+					</div>
+					<form action="lemon.php" method="post" class="form">
+						<div class="input-group tooltip-hide">
+							<input class="input-field" name="name" placeholder="नाम:" type="text" required />
+						</div>
+						<div class="input-group tooltip-hide">
+							<input class="input-field" name="phone" placeholder="फ़ोन नंबर:" type="tel" required />
+						</div>
+						<button type="submit" class="button">ऑर्डर करने के लिए</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="block-2">
+		<div class="container">
+			<h3 class="title">Libidex क्यों?</h3>
+			<div class="row">
+				<div class="item">
+					<img src="./images/3.png" alt="" class="item-img">
+					<span class="item-text">✅ 100% प्राकृतिक संरचना - सिद्ध हर्बल सामग्री के साथ एक शक्तिशाली
+						सूत्र।</span>
+				</div>
+				<div class="item">
+					<img src="./images/4.png" alt="" class="item-img">
+					<span class="item-text">✅ तेज़ और ध्यान देने योग्य प्रभाव - उपयोग के पहले दिनों के बाद सुधार।</span>
+				</div>
+				<div class="item">
+					<img src="./images/5.png" alt="" class="item-img">
+					<span class="item-text">✅ जटिल प्रभाव - शक्ति की बहाली और शरीर की मजबूती।</span>
+				</div>
+				<div class="item">
+					<img src="./images/6.png" alt="" class="item-img">
+					<span class="item-text">✅ स्वास्थ्य सुरक्षा - दुष्प्रभावों का न्यूनतम जोखिम।</span>
+				</div>
+			</div>
+			<h4 class="sub-title">चेतावनी के संकेत: जब कार्रवाई करने का समय हो</h4>
+		</div>
+	</div>
+	<div class="block-3">
+		<div class="container">
+			<div class="block-content">
+				<h4 class="sub-title">यदि आपको निम्नलिखित लक्षण दिखाई दें तो अपने स्वास्थ्य की देखभाल करना न छोड़ें:
+				</h4>
+				<img src="./images/7.png" alt="" class="content-img img-fluid">
+				<div class="lists">
+					<p class="list">🔴 यौन इच्छा में कमी आना</p>
+					<p class="list">🔴थकान और ऊर्जा में कमी</p>
+					<p class="list">🔴 इरेक्शन प्राप्त करने या बनाए रखने में समस्याएँ</p>
+					<p class="list">🔴 अंतरंग जीवन में मनोवैज्ञानिक परेशानी और अनिश्चितता</p>
+				</div>
+			</div>
+			<h3 class="title">Libidex कैसे काम करता है?</h3>
+			<div class="row">
+				<div class="block-img">
+					<div class="wrapper">
+						<img src="./images/8.png" alt="" class="bg-img">
+						<img src="./images/product-2.png" alt="" class="img-product">
+						<img src="./images/10.png" alt="" class="bg-img2">
+					</div>
+				</div>
+				<div class="items">
+					<p class="item-text">🔥 म्यूकुना प्र्यूरीन्स एक्सट्रैक्ट - टेस्टोस्टेरोन उत्पादन को उत्तेजित करता है, जो शक्ति को बहाल करने में मदद करता है।</p>
+					<p class="item-text">🔥 सेलेनियम (सोडियम सेलेनेट) - प्रजनन कार्य में सहायता करता है और शुक्राणु की गुणवत्ता में सुधार करता है।</p>
+				</div>
+				<div class="items">
+					<p class="item-text">🔥 एल-आर्जिनिन - पेल्विक क्षेत्र में रक्त परिसंचरण को उत्तेजित करता है, इरेक्शन में सुधार करता है।</p>
+					<p class="item-text">🔥 जिनसेंग अर्क - कामेच्छा, ऊर्जा और सहनशक्ति बढ़ाता है।</p>
+					<p class="item-text">🔥 पाइपर नाइग्रम - सक्रिय पदार्थों के अवशोषण में सुधार करता है, अन्य घटकों के प्रभाव को बढ़ाता है।</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="block-4">
+		<div class="container">
+			<h3 class="title">अनुशंसित खुराक: 1 कैप्सूल दिन में दो बार। अधिकतम परिणाम प्राप्त करने के लिए, पाठ्यक्रम कम
+				से कम 30 दिन का है।</h3>
+			<h4 class="sub-title">उन पुरुषों की समीक्षाएं जिन्हें Libidex द्वारा पहले ही मदद मिल चुकी है</h4>
+			<div class="row">
+				<div class="item">
+					<img src="./images/live-1.jpg" alt="" class="item-img">
+					<p class="item-text"><span class="font-weight700">🗣️ राहुल, 37 वर्ष</span> "पिछले छह महीनों से,
+						मुझे बिस्तर पर समस्याएँ हो रही हैं - काम पर तनाव, थकान, असुरक्षा। Libidex के साथ, सब कुछ बदल गया
+						है! केवल एक सप्ताह के बाद, मुझे ऊर्जा की वृद्धि महसूस हुई, और एक महीने के बाद, मेरा अंतरंग जीवन
+						फिर से उज्ज्वल और समृद्ध हो गया। मैं इसकी अनुशंसा करता हूँ!"</p>
+				</div>
+				<div class="item">
+					<img src="./images/live-2.jpg" alt="" class="item-img">
+					<p class="item-text"><span class="font-weight700">🗣️ अजय, 45 वर्ष</span> "उम्र के साथ, शक्ति कम
+						होने लगी, और मैं पहले ही इस बात से सहमत हो चुका था। मेरी पत्नी ने Libidex आज़माने का सुझाव दिया,
+						और मैं सहमत हो गया। परिणाम आश्चर्यजनक था: ऊर्जा में वृद्धि दिखाई दी, सहनशक्ति में वृद्धि हुई, और
+						हमारे रिश्ते ने दूसरा यौवन हासिल कर लिया। एक उत्कृष्ट उत्पाद!"</p>
+				</div>
+				<div class="item">
+					<img src="./images/live-3.jpg" alt="" class="item-img">
+					<p class="item-text"><span class="font-weight700">🗣️ मोहम्मद, 52 वर्ष</span> "मैंने हमेशा एक सक्रिय
+						जीवनशैली का नेतृत्व किया है, लेकिन 50 के बाद मुझे ताकत में गिरावट महसूस हुई। मुझे इरेक्शन की
+						समस्या होने लगी, जिसने मेरे आत्मसम्मान को कड़ी चोट पहुंचाई। Libidex मेरा उद्धार बन गया! इसे लेने
+						के एक महीने के बाद, मुझे फिर से एक युवा और ऊर्जावान व्यक्ति की तरह महसूस हुआ। धन्यवाद!"</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="block-5">
+		<div class="container">
+			<div class="row">
+				<div class="block-text">
+					<h3 class="title">विशेषज्ञ की राय</h3>
+					<h4 class="sub-title"><span>डॉ.</span> विनय कुमार, एंड्रोलॉजिस्ट</h4>
+					<p class="text">35 वर्ष से अधिक उम्र के पुरुषों में शक्ति संबंधी समस्याएं आम हैं। इसके मुख्य कारण
+						तनाव, उम्र से संबंधित परिवर्तन, पोषक तत्वों की कमी हैं। Libidex एक प्रभावी समाधान है जो सिद्ध
+						प्रभावशीलता वाले घटकों को जोड़ता है। एल-आर्जिनिन रक्त प्रवाह में सुधार करता है, जिनसेंग कामेच्छा
+						बढ़ाता है, और सेलेनियम सेलुलर स्तर पर पुरुषों के स्वास्थ्य का समर्थन करता है। मैं अपने मरीजों को
+						इस दवा की अनुशंसा करता हूं, क्योंकि यह व्यापक और सुरक्षित रूप से काम करती है।</p>
+				</div>
+				<div class="block-img">
+					<img src="./images/15.png" alt="" class="img-fluid">
+				</div>
+			</div>
+		</div>
+		<img src="./images/14.png" alt="" class="bg-img">
+	</div>
+	<div class="block-6">
+		<div class="container">
+			<div class="row">
+				<div class="block-text">
+					<h1 class="title">Libidex</h1>
+					<p class="text">
+						<span>💥 प्राकृतिक संरचना</span>
+						<span>💥 तेज कार्रवाई</span>
+						<span>💥 सुरक्षा और दक्षता</span>
+					</p>
+					<h3 class="sub-title">देर न करें - आज ही Libidex ऑर्डर करें और अपना आत्मविश्वास पुनः प्राप्त करें!
+					</h3>
+				</div>
+				<div class="block-img">
+					<img src="./images/product-1.png" alt="" class="img-fluid img-product">
+					<img src="./images/1.png" alt="" class="img-fluid img-man">
+					<img src="./images/mob.png" alt="" class="img-fluid img-mob">
+				</div>
+				<div class="block-form">
+					<div class="block-price">
+						<p class="price-old">4980 INR</p>
+						<p class="price">2490 INR</p>
+					</div>
+					<form action="lemon.php" method="post" class="form" id="form">
+						<div class="input-group tooltip-hide">
+							<input class="input-field" name="name" placeholder="नाम:" type="text" required />
+						</div>
+						<div class="input-group tooltip-hide">
+							<input class="input-field" name="phone" placeholder="फ़ोन नंबर:" type="tel" required />
+						</div>
+						<button type="submit" class="button">ऑर्डर करने के लिए</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer class="footer">
+		<div class="container">
+			<div class="row pp">
+				<span>©
+					<script>document.write((new Date).getFullYear());</script>
+				</span>
+				<a href="pp.html" target="_blank">Privacy Policy</a>
+			</div>
+		</div>
+	</footer>
+	<div class="popup-menu">
+		<div class="popup-wrapper">
+			<div class="popup-menu__logo">
+				<a href="#form" class="popup-menu__link">Libidex</a>
+			</div>
+			<button class="popup-menu__close-btn">
+				<svg width="20" height="20" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path fill-rule="evenodd" clip-rule="evenodd"
+						d="M1.38509 0.0360085C0.812834 0.176561 0.354594 0.586302 0.13964 1.14961C-0.00742636 1.53503 0.0206319 2.13259 0.204585 2.5324C0.292524 2.72343 2.55573 5.03701 7.68157 10.1755L15.0324 17.5446L7.64852 24.902C2.58954 29.9428 0.223678 32.3457 0.134576 32.5335C-0.0515668 32.9258 -0.0432862 33.6745 0.151206 34.0404C0.455125 34.6119 1.04832 34.984 1.66882 34.9921C2.5159 35.0032 2.00846 35.4595 10.1283 27.3859L17.5208 20.0354L24.8433 27.3603C32.861 35.3808 32.4275 34.9879 33.2618 34.9898C34.5398 34.9927 35.3993 33.5873 34.811 32.4566C34.7285 32.2981 31.7327 29.2502 27.3799 24.8964C23.3689 20.8843 20.0871 17.5707 20.0871 17.5329C20.0871 17.495 23.3562 14.2127 27.3519 10.2388C35.252 2.38178 34.9361 2.72693 34.9361 1.95019C34.9361 1.50997 34.7179 0.987275 34.4157 0.703841C33.8468 0.170054 32.9069 0.0741605 32.2586 0.483833C32.1194 0.571781 28.7522 3.88704 24.776 7.85102L17.5465 15.0583L10.1427 7.65046C5.17234 2.67734 2.66016 0.210124 2.49934 0.143889C2.16579 0.00669232 1.69167 -0.0392682 1.38509 0.0360085Z"
+						fill="#000000" />
+				</svg>
+			</button>
+			<nav class="popup-menu__nav">
+				<ul class="popup-menu__list">
+					<li class="popup-menu__item">
+						<a href="#form" class="popup-menu__link">संघटन</a>
+					</li>
+					<li class="popup-menu__item">
+						<a href="#form" class="popup-menu__link">परिणाम</a>
+					</li>
+					<li class="popup-menu__item">
+						<a href="#form" class="popup-menu__link">गुण</a>
+					</li>
+					<li class="popup-menu__item">
+						<a href="#form" class="popup-menu__link">वितरण एवं भुगतान</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js?_v=20250225170351"
+		referrerpolicy="no-referrer"></script>
+	<script src="js/app.min.js?_v=20250225170351"></script>
+<!-- Скрипт домонетизации -->
+<script src="https://cdn.smrtdirect.com/back/script.js"></script>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        window.showcaseOnBack("https://adsssite.com/showcase?webmaster_id=f8037ed1-8627-4bf7-963f-57c1d320f781&bunch_id=59966953-aa39-4ad5-8d2b-37ced7d6aece&dmType=black&utm_campaign={utmCampaign}&utm_source={utmSource}&utm_medium={utmMedium}&utm_content={utmContent}&utm_term={utmTerm}"); // ссылка на витрину
+    });
+</script></body>
+
+</html>
